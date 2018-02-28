af1c816f51668a26ac5e290646d980551f547c5f
liuhepeng <liuhepeng@besttmkj.com>
2018-02-28 15:47:32 +0800

    Initial Commit

---
+++ Lib_Study_Xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lib_Study_Xcode
+//
+//  Created by Jerry on 28/02/2018.
+//  Copyright © 2018 Jerry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
